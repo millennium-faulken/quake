9ec1d3974be78a571b88fb4dd64652c5f6a3ae16
millennium-faulken <justin.faulkenberry@gmail.com>
2022-05-12 14:16:42 -0500

    no curl

--- api.php
+++ api.php
@@ -1,27 +1,6 @@
 <?php
-// $getUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=5";
-
-// $getQuake = file_get_contents($getUrl);
-
-// echo $getQuake;
-
 $getUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=5";
 
-$get = curl_init();
+$getQuake = file_get_contents($getUrl);
 
-curl_setopt($get, CURLOPT_URL, $getUrl);
-curl_setopt($get, CURLOPT_POST, 1);
-curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($get, CURLOPT_SSL_VERIFYPEER, 0);
-
-if (curl_errno($get)) {
-    echo curl_error($get);
-    curl_close($get);
-    exit();
-}
-
-$server_output = curl_exec($get);
-
-curl_close($get);
-
-echo $server_output;
+echo $getQuake;
