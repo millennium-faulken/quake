bd1cddc547350b8c017819cb2a34b0b088d2f8a9
millennium-faulken <justin.faulkenberry@gmail.com>
2022-05-16 17:36:54 -0500

    updates - magnitude search function

--- api.php
+++ api.php
@@ -1,21 +1,26 @@
 <?php
 
-$getUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=5.5";
+if (isset($_POST['mag'])) {
 
-$get = curl_init();
+    $mag = $_POST['mag'];
 
-curl_setopt($get, CURLOPT_URL, $getUrl);
-curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($get, CURLOPT_SSL_VERIFYPEER, 0);
+    $getUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=$mag";
 
-if (curl_errno($get)) {
-    echo curl_error($get);
+    $get = curl_init();
+
+    curl_setopt($get, CURLOPT_URL, $getUrl);
+    curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($get, CURLOPT_SSL_VERIFYPEER, 0);
+
+    if (curl_errno($get)) {
+        echo curl_error($get);
+        curl_close($get);
+        exit();
+    }
+
+    $server_output = curl_exec($get);
+
     curl_close($get);
-    exit();
+
+    echo $server_output;
 }
-
-$server_output = curl_exec($get);
-
-curl_close($get);
-
-echo $server_output;
--- script.js
+++ script.js
@@ -2,47 +2,59 @@ $(document).ready(function () {
   getQuakes();
 
   function getQuakes() {
-    $.ajax({
-      type: "POST",
-      url: "api.php",
-      beforeSend: function () {
-        $("#loading").show();
-        $("#table").hide();
-      },
-      complete: function () {
-        $("#loading").hide();
-        $("#table").show();
-      },
-      success: function (response) {
-        JSON.parse(response).features.map(function (quake) {
-          var item = [];
-          const date = new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
+    $("#loading").hide();
+    $("#table").hide();
+
+    $("#submit").click(function () {
+      var mag = $("#mag").val();
+      console.log($("#mag").val());
+
+      $.ajax({
+        type: "POST",
+        url: "api.php",
+        data: {
+          mag: mag,
+        },
+        beforeSend: function () {
+          $("#loading").show();
+          $("#table").hide();
+          $("#quakes").empty();
+        },
+        complete: function () {
+          $("#loading").hide();
+          $("#table").show();
+        },
+        success: function (response) {
+          JSON.parse(response).features.map(function (quake) {
+            var item = [];
+            const date = new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            });
+            item.push(
+              "<tr>" +
+                '<td class="place">' +
+                quake.properties.place +
+                "</td>" +
+                '<td class="mag">' +
+                parseFloat(quake.properties.mag).toFixed(2) +
+                "</td>" +
+                '<td class="time">' +
+                date.format(quake.properties.time) +
+                "</td>" +
+                "<tr/>"
+            );
+            return $("#quakes").append(item);
           });
-          item.push(
-            "<tr>" +
-              '<td class="place">' +
-              quake.properties.place +
-              "</td>" +
-              '<td class="mag">' +
-              quake.properties.mag +
-              "</td>" +
-              '<td class="time">' +
-              date.format(quake.properties.time) +
-              "</td>" +
-              "<tr/>"
-          );
-          return $("#quakes").append(item);
-        });
-      },
-      error: function (status) {
-        console.log(status);
-      },
+        },
+        error: function (status) {
+          console.log(status);
+        },
+      });
     });
   }
 });
